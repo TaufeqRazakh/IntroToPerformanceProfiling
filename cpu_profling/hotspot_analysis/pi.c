08061f9d21cddfc6f668bda433babe66da5f18da
TaufeqRazakh <taufeqmdr@gmail.com>
2021-11-12 06:07:19 -0800

    moved files for better segmentation

---
+++ cpu_profling/hotspot_analysis/pi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define NBIN 100000
+int main() {
+	long long i;
+	double step,x,sum=0.0,pi;
+
+	step = 1.0/NBIN;
+	for (i=0; i<NBIN; i++) {
+		x = (i+0.5)*step;
+		sum += 4.0/(1.0+x*x);
+	}
+	pi = sum*step;
+	printf("PI = %f\n",pi);
+	return 0;
+}
